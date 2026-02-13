9ac0cad544e927283ab86319ef823f5195baeef0
rehmeloz <renatamelo.pro@outlook.com>
2026-02-13 15:57:03 -0300

    adicionando enumeradores do backend

---
+++ ControleGastosResidenciais.Server/Enums/FinalidadeCategoria.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleGastosResidenciais.Server.Enums;
+
+// Definindo finalidades de categoria, essas finalidades são utilizadas no momento de criação de uma categoria
+public enum FinalidadeCategoria
+{
+    Despesa = 1,
+    Receita = 2,
+    Ambas = 3
+}
---
+++ ControleGastosResidenciais.Server/Enums/TipoTransacao.cs
@@ -0,0 +1,8 @@
+﻿namespace ControleGastosResidenciais.Server.Enums;
+
+// Definindo de tipo de transações, utilizada no momento da criação de uma nova transação
+public enum TipoTransacao
+{
+    Despesa = 1,
+    Receita = 2
+}
