b0150d3aed8ad7d4a7849db30277942cb97361a6
rehmeloz <renatamelo.pro@outlook.com>
2026-06-29 17:38:20 -0300

    Adiciona AppDbContext para inicio de transição de ORM Dapper para EF Core

---
+++ ControleGastosResidenciais.Server/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace ControleGastosResidenciais.Server.Data;
+using ControleGastosResidenciais.Server.Data.Mappings;
+using ControleGastosResidenciais.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Pessoa> Pessoas { get; set; }
+    public DbSet<Transacao> Transacoes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CategoriaMapping());
+        modelBuilder.ApplyConfiguration(new PessoaMapping());
+        modelBuilder.ApplyConfiguration(new TransacoesMapping());
+    }
+}
