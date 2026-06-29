56a6d8baa82e205982558af9b008e1b4fba861a0
rehmeloz <renatamelo.pro@outlook.com>
2026-06-29 17:53:49 -0300

    Adiciona pasta Migrations e recria banco com as Migrations do EF Core

---
+++ ControleGastosResidenciais.Server/Data/Migrations/20260629205109_IniciarBanco.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControleGastosResidenciais.Server.Data
+{
+    /// <inheritdoc />
+    public partial class IniciarBanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descricao = table.Column<string>(type: "TEXT", maxLength: 400, nullable: false),
+                    Finalidade = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Idade = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descricao = table.Column<string>(type: "TEXT", maxLength: 400, nullable: false),
+                    Valor = table.Column<decimal>(type: "REAL", nullable: false),
+                    Tipo = table.Column<int>(type: "INTEGER", nullable: false),
+                    IdPessoa = table.Column<int>(type: "INTEGER", nullable: false),
+                    IdCategoria = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transacoes_Categorias_IdCategoria",
+                        column: x => x.IdCategoria,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Transacoes_Pessoas_IdPessoa",
+                        column: x => x.IdPessoa,
+                        principalTable: "Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_IdCategoria",
+                table: "Transacoes",
+                column: "IdCategoria");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_IdPessoa",
+                table: "Transacoes",
+                column: "IdPessoa");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transacoes");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
